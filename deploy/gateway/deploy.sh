3db8042e16629ff198f8ac3387fad403b512653d
RaidasGrisk <mrraidas@gmail.com>
2020-07-15 16:52:18 +0300

    fix typo

--- deploy/gateway/deploy.sh
+++ deploy/gateway/deploy.sh
@@ -3,5 +3,5 @@
 # execute this to build and deploy the image to google cloud run
 # execute this from the main directory of the project
 docker build -f deploy/gateway/Dockerfile . --tag gcr.io/namesapi-1581010760883/namespy-api
-docker push gcr.io/namesapi-1581010760883/amespy-api
-gcloud run deploy --image gcr.io/namesapi-1581010760883/amespy-api --platform managed --region europe-north1
+docker push gcr.io/namesapi-1581010760883/namespy-api
+gcloud run deploy --image gcr.io/namesapi-1581010760883/namespy-api --platform managed --region europe-north1
