953fe18d242696ea946f209258198206fda27f86
RaidasGrisk <mrraidas@gmail.com>
2020-09-25 15:34:03 +0300

    retry bug fix

--- api/data_sources/async_utils.py
+++ api/data_sources/async_utils.py
@@ -3,8 +3,6 @@ import asyncio
 from log_cofig import logger
 
 
-# TODO: if requests fails it returns ClientHttpProxyError object
-# TODO: can not pass it further, should fix it here by making the call again and retrieving the response
 def make_async_requests(urls, proxies=None):
 
     proxy = proxies.get('http') if proxies else ''
@@ -16,15 +14,16 @@ def make_async_requests(urls, proxies=None):
 
         # setting 5 retries in case of proxy fail
         # sometimes the url (due to proxies) is unreachable
-        # TODO: should specify the exact exception(s) as this could be due to other reasons
         for i in range(5):
             logger.info(f'Gathering single async request: {i}')
             try:
                 async with session.get(url, proxy=proxy, timeout=timeout) as response:
                     logger.info(f'Gathering single async request: successfully received a response')
                     return await response.text()
-            except Exception as e:  # TODO: aiohttp.ClientError
-                logger.exception(f'Single async requests exception: {str(e)}')
+            except Exception as e:
+                # TODO: setting to logger.exception breaks everything !?
+                # by not continuing the for loop and stopping just before the below line
+                logger.info(f'Single async requests exception: {repr(e)}')
 
     async def fetch_all(urls, loop):
         async with aiohttp.ClientSession(loop=loop) as session:
@@ -40,73 +39,8 @@ def make_async_requests(urls, proxies=None):
     return responses
 
 # ----------------- #
-#
-#
-# # ----------- #
-# # call APIs to collect data
-# # https://stackoverflow.com/questions/51726007/fetching-multiple-urls-with-aiohttp-in-python
-# import aiohttp
-# import asyncio
-#
-# proxies = {'http': 'http://f3t0zfun:03qLGKGeOdrkbiTE@proxy.proxy-cheap.com:31112',
-#            'https': 'http://f3t0zfun:03qLGKGeOdrkbiTE@proxy.proxy-cheap.com:31112'}
-#
-# proxy = proxies.get('http')
-#
-# async def fetch(session, url, proxy):
-#     async with session.get(url, proxy=proxy) as response:
-#         return await response.json()
-#
-#
-# async def fetch_all(urls, loop):
-#     async with aiohttp.ClientSession(loop=loop, proxy=proxy) as session:
-#         results = await asyncio.gather(*[fetch(session, url, proxy) for url in urls], return_exceptions=True)
-#         return results
-#
-#
-# loop = asyncio.get_event_loop()
-# urls = ['http://ip-api.com/json/' for _ in range(3)]
-# responses = loop.run_until_complete(fetch_all(urls, loop))
-# [print(i) for i in responses]
-#
-#
-# # -------- #
-# # https://stackoverflow.com/questions/61537570/how-do-i-make-parallel-async-http-requests-using-httpx-versus-aiohttp-in-pytho
-# import asyncio
-# import httpx
-#
-# proxies = {'http': 'http://f3t0zfun:03qLGKGeOdrkbiTE@proxy.proxy-cheap.com:31112',
-#            'https': 'http://f3t0zfun:03qLGKGeOdrkbiTE@proxy.proxy-cheap.com:31112'}
-#
-#
-# async def fetch(session, url):
-#     response = await session.request(method='GET', url=url)
-#     return response
-#
-#
-# async def get_requests_asynch(urls):
-#     async with httpx.AsyncClient(proxies=proxies) as session:
-#         htmls = await asyncio.gather(*[fetch(session, url) for url in urls])
-#     return htmls
-#
-#
-# loop = asyncio.get_event_loop()
-# urls = ['http://ip-api.com/json/' for _ in range(3)]
-# htmls = loop.run_until_complete(get_requests_asynch(urls))
-# [print(i.text) for i in htmls]
-#
-# # -------- #
-# # https://stackoverflow.com/questions/30837839/how-can-i-set-a-single-proxy-for-a-requests-session-object
-# import requests
-#
-# proxies = {'http': 'http://f3t0zfun:03qLGKGeOdrkbiTE@proxy.proxy-cheap.com:31112',
-#            'https': 'http://f3t0zfun:03qLGKGeOdrkbiTE@proxy.proxy-cheap.com:31112'}
-#
-# urls = ['http://ip-api.com/json/' for _ in range(3)]
-#
-# session = requests.session()
-# session.proxies.update(proxies)
-# for url in urls:
-#     r = session.get(url)
-#     print(r.json())
+# testing
 
+# logger.handlers[0].flush()
+# make_async_requests(['http://www.dasdasdasdasdasd.asd', 'https://www.namespy.dev'])
+# [print(i) for i in logger.handlers[0].log]
