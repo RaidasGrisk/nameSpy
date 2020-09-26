6f26d0b4005c6d6a7d348c3b3c68a3436b90e1fe
RaidasGrisk <mrraidas@gmail.com>
2020-09-26 10:50:19 +0300

    comment out unused fn

--- api/data_sources/instagram.py
+++ api/data_sources/instagram.py
@@ -16,54 +16,10 @@ https://github.com/ping/instagram_private_api/blob/54427574583d33544c006c9f6a13c
 
 """
 
-import requests
-from bs4 import BeautifulSoup
 import json
 from data_sources.async_utils import make_async_requests
 from data_sources.requests_utils import requests_retry_session
 from log_cofig import logger
-
-# The following func is not longer used as there is a better solution!
-# TODO: hard to replicate but sometimes this parse does not do the job
-#  guess is that the response html structure depends on the caller location
-#  when doing this through proxy the response structure is not parsed sometimes
-#  maybe fix the proxy location?
-#  another solution would be to work with async calls and set single proxy for whole batch
-def parse_html_to_user_info(html):
-    """
-    Parses data from 'https://www.instagram.com/{username}/?__a=1'
-    """
-
-    output = {}
-    soup = BeautifulSoup(html, 'html.parser')
-
-    # TODO: private public?
-    # TODO: description
-    for item in soup.findAll('meta', attrs={'name': 'description', 'content': True}):
-        content = item.get('content')
-        if 'Followers' in content:
-            words = content.split()
-            # TODO: this does not work all the time
-            try:
-                output['followers_count'] = int(words[0].replace('k', '00').replace(',', '').replace('.', ''))
-                output['following_count'] = int(words[2].replace('k', '000').replace(',', '').replace('.', ''))
-                output['posts_count'] = int(words[4].replace('k', '000').replace(',', '').replace('.', ''))
-            except:
-                print('Failed to parse IG user info, now trying another html structure')
-                continue
-            break
-
-    # if nothing was found then profile is set to private and html is structured differently
-    if not output:
-        js_items = soup.findAll('script', attrs={'type': 'text/javascript'})
-        for item in js_items:
-            # TODO: somehow the following line only works with beautifulsoup4==4.8.2
-            if 'edge_followed_by' in item.text:
-                js_json = json.loads(item.text.replace('window._sharedData = ', '')[:-1])
-                output['followers_count'] = int(js_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_followed_by']['count'])
-                output['following_count'] = int(js_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_follow']['count'])
-                output['posts_count'] = int(js_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['count'])
-    return output
 
 
 def parse_json_to_user_info(user_info):
@@ -117,3 +73,46 @@ def get_instagram_users(input, proxies):
     output['users'] = users
 
     return output
+
+
+# # The following func is not longer used as there is a better solution!
+# # TODO: hard to replicate but sometimes this parse does not do the job
+# #  guess is that the response html structure depends on the caller location
+# #  when doing this through proxy the response structure is not parsed sometimes
+# #  maybe fix the proxy location?
+# #  another solution would be to work with async calls and set single proxy for whole batch
+# def parse_html_to_user_info(html):
+#     """
+#     Parses data from 'https://www.instagram.com/{username}/?__a=1'
+#     """
+#
+#     output = {}
+#     soup = BeautifulSoup(html, 'html.parser')
+#
+#     # TODO: private public?
+#     # TODO: description
+#     for item in soup.findAll('meta', attrs={'name': 'description', 'content': True}):
+#         content = item.get('content')
+#         if 'Followers' in content:
+#             words = content.split()
+#             # TODO: this does not work all the time
+#             try:
+#                 output['followers_count'] = int(words[0].replace('k', '00').replace(',', '').replace('.', ''))
+#                 output['following_count'] = int(words[2].replace('k', '000').replace(',', '').replace('.', ''))
+#                 output['posts_count'] = int(words[4].replace('k', '000').replace(',', '').replace('.', ''))
+#             except:
+#                 print('Failed to parse IG user info, now trying another html structure')
+#                 continue
+#             break
+#
+#     # if nothing was found then profile is set to private and html is structured differently
+#     if not output:
+#         js_items = soup.findAll('script', attrs={'type': 'text/javascript'})
+#         for item in js_items:
+#             # TODO: somehow the following line only works with beautifulsoup4==4.8.2
+#             if 'edge_followed_by' in item.text:
+#                 js_json = json.loads(item.text.replace('window._sharedData = ', '')[:-1])
+#                 output['followers_count'] = int(js_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_followed_by']['count'])
+#                 output['following_count'] = int(js_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_follow']['count'])
+#                 output['posts_count'] = int(js_json['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['count'])
+#     return output
