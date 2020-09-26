3de0fe255147d0244ddf862262088265ac7ef29f
RaidasGrisk <mrraidas@gmail.com>
2020-09-26 18:14:18 +0300

    remove tor; socialscore -> webscore rename; other deply updates

--- deploy/job_title/deploy.sh
+++ deploy/job_title/deploy.sh
@@ -4,5 +4,7 @@
 # execute this from the main directory of the project
 docker build -f deploy/job_title/Dockerfile . --tag gcr.io/namesapi-1581010760883/jobtitle
 docker push gcr.io/namesapi-1581010760883/jobtitle
+
+gcloud config set project namesapi-1581010760883
 gcloud run deploy --image gcr.io/namesapi-1581010760883/jobtitle --platform managed --region europe-north1
 gcloud run services update jobtitle --memory 2G  --platform managed --region europe-north1 --max-instances 5
--- deploy/web_score/deploy.sh
+++ deploy/web_score/deploy.sh
@@ -2,7 +2,9 @@
 
 # execute this to build and deploy the image to google cloud run
 # execute this from the main directory of the project
-docker build -f deploy/web_score/Dockerfile . --tag gcr.io/namesapi-1581010760883/socialscore
-docker push gcr.io/namesapi-1581010760883/socialscore
-gcloud run deploy --image gcr.io/namesapi-1581010760883/socialscore --platform managed --region europe-north1
-gcloud run services update socialscore --memory 2G --platform managed --region europe-north1 --max-instances 5
+docker build -f deploy/web_score/Dockerfile . --tag gcr.io/namesapi-1581010760883/webscore
+docker push gcr.io/namesapi-1581010760883/webscore
+
+gcloud config set project namesapi-1581010760883
+gcloud run deploy --image gcr.io/namesapi-1581010760883/webscore --platform managed --region europe-north1
+gcloud run services update webscore --memory 2G --platform managed --region europe-north1 --max-instances 5
